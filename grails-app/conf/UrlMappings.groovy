25442b7dd37f9b3cf43051c17b0a9df08b8fc5ae
jithin <jithinpjose93@gmail.com>
2020-07-05 00:49:51 -0700

    Second change

--- grails-app/conf/UrlMappings.groovy
+++ grails-app/conf/UrlMappings.groovy
@@ -7,7 +7,8 @@ class UrlMappings {
             }
         }
 
-        "/"(view:"/index")
+        //"/"(view:"/index")
+		"/"(view:"/first")
         "500"(view:'/error')
 	}
 }
---
+++ grails-app/views/first.gsp
@@ -0,0 +1,8 @@
+<html>
+<head>
+
+</head>
+<body>
+<h1>First heading</h1>
+</body>
+</html>
